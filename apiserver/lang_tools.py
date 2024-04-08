be6449f447336dc8a3079d594764efa519fa41e3
yoyofx <zl.hxd@hotmail.com>
2024-04-08 23:06:58 +0800

    os 依赖

--- apiserver/lang_tools.py
+++ apiserver/lang_tools.py
@@ -1,7 +1,10 @@
 from langchain.agents import tool
 from datetime import date
 import subprocess
-from appscript import app
+import os
+
+if os.name == 'Darwin':
+    from appscript import app
 
 
 @tool
@@ -31,16 +34,17 @@ def note(input_string: str) -> str:
     Args:
         input_string: string 打开记事本后输入的内容
     """
-    if input_string != "":
-        notes = app('Notes')
-        # 打开Notes应用程序
-        notes.activate()
-        # # 创建一个新笔记
-        new_note = notes.make(new='note')
-        print(new_note)
-        new_note.body.set(input_string)
-    else :
-        return "主人,你要记录的内容是什么呢? 我帮你记录哦"
+    if os.name == 'Darwin':
+        if input_string != "":
+            notes = app('Notes')
+            # 打开Notes应用程序
+            notes.activate()
+            # # 创建一个新笔记
+            new_note = notes.make(new='note')
+            print(new_note)
+            new_note.body.set(input_string)
+        else :
+            return "主人,你要记录的内容是什么呢? 我帮你记录哦"
 
     # #Reminders 提醒
     
@@ -53,20 +57,19 @@ def music(input_string: str) -> str:
     Args:
         input_string: string 要搜索的音乐/歌曲/要播放的音乐/歌曲
     """
-    itunes = app('Music')
-    itunes.activate()
+    if os.name == 'Darwin':
+        itunes = app('Music')
+        itunes.activate()
 
-    if input_string != "":
-        search_results = itunes.playlist.search(input_string)
-        if search_results:
-            # 获取搜索结果中的第一首歌曲
-            first_track = search_results[0]
+        if input_string != "":
+            search_results = itunes.playlist.search(input_string)
+            if search_results:
+                # 获取搜索结果中的第一首歌曲
+                first_track = search_results[0]
 
-            # 播放搜索结果中的第一首歌曲
-            itunes.play(first_track)
-    else:
-        itunes.play()
-
-
+                # 播放搜索结果中的第一首歌曲
+                itunes.play(first_track)
+        else:
+            itunes.play()
 
     return "主人已打开音乐播放器"
